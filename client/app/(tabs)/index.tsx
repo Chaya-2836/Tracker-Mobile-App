3547f1bb4a83468c3cbeaf7a280e505b985ac66e
HadasZada <h3188130@gmail.com>
2025-06-15 19:01:01 +0300

    chore: install native-base and related dependencies

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
 import { WelcomeMessage } from "@/components/WelcomeScreen";
+import { NativeBaseProvider, Box, Text } from 'native-base';
 import React from "react";
 
 
 export default function HomeScreen() {
   return (
-    <WelcomeMessage/>
+    <NativeBaseProvider>
+      <Box flex={1} justifyContent="center" alignItems="center">
+        <WelcomeMessage/>
+      </Box>
+    </NativeBaseProvider>
   )
 }
