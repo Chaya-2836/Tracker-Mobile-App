0bd8fd5e2eda5f5f2ce619760e6274c812893439
HadasZada <h3188130@gmail.com>
2025-06-15 17:49:56 +0300

    Add a welcome message

---
+++ client/components/WelcomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native"
+
+
+export function WelcomeMessage(){
+    return(
+        <View>
+            <Text>Hello For You!!!!</Text>
+        </View>
+    )
+}
