9e4f3aaed0bd7aed9ecc309cb14b7c04b1ef1f16
HadasZada <h3188130@gmail.com>
2025-06-15 19:15:59 +0300

    add styles folder and initial styling file

--- client/components/WelcomeScreen.tsx
+++ client/components/WelcomeScreen.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native"
+import styles from "../app/styles/welcomeScreen"
 
 
 export function WelcomeMessage(){
     return(
         <View>
-            <Text>Hello For You!!!!</Text>
+            <Text style={styles.text}>Hello For You😍😍😍</Text>
         </View>
     )
 }
