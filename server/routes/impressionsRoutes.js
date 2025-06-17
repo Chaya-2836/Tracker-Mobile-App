e1a99058302154e4d818d49851bf2c3320fa152f
chayale <0556762836c@gmail.com>
2025-06-17 23:34:18 +0300

    Connect client and server and Add weekly impression trend API and client chart

--- server/routes/impressionsRoutes.js
+++ server/routes/impressionsRoutes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const impressionsController = require("../controllers/impressionsController");
 
-
 router.get("/allImpressions", impressionsController.getAllImpressions);
+router.get("/ImpressionsByCampaign", impressionsController.getImpressionsByCampaign_name);
 
 module.exports = router;
-
