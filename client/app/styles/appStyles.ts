104f7d8afef5c444f876a4d290b88f868ecbb885
michalkarmely <karmelymichal@gmail.com>
2025-06-17 16:26:44 +0300

    visual components addition

---
+++ client/app/styles/appStyles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f7f9fc',
+    paddingTop: 20,
+  },
+  header: {
+    fontSize: 26,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginVertical: 20,
+    color: '#2c3e50',
+  },
+});
+
+export default styles;
