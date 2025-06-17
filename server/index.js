5c46848951bb4d25ce25d221a1b054f3a96d0ac9
HadasZada <h3188130@gmail.com>
2025-06-17 13:33:07 +0300

    feat: add controllers and routes directories, implement daily impressions count function

--- server/index.js
+++ server/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const { BigQuery } = require("@google-cloud/bigquery");
+//צריך להוסיף את ניתוב לקובץ ההרשאות
+const bigquery = new BigQuery({
+    keyFilename: ""
+});
+const nameDB = "platform-hackaton-2025";
+module.exports = { bigquery, nameDB };
+
+
+const impressionsRouter = require("./routes/impressionsRoutes")
+
+app.use("/impressions",impressionsRouter);
+
 app.get('/user', (req, res) => {
   res.json({ name: 'John Doe' }); 
 });
