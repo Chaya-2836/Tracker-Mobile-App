e2863cb825de3d729ab79277e9703f30fdaaa6c0
EstiZelt <e0527185161@gmail.com>
2025-06-18 11:01:43 +0300

    add todayClicks

--- server/controllers/clicksController.js
+++ server/controllers/clicksController.js
@@ -61,3 +61,72 @@ exports.getClicksByCampaign_name = async (req, res) => {
     res.status(500).json({ error: "אירעה שגיאה בעת ביצוע השאילתה" });
   }
 };
+exports.getTodayClicksByCampaign = async (req, res) => {
+    try {
+      const campaign_name = req.query.campaign_name;
+  
+      if (!campaign_name) {
+        return res.status(400).send("יש לספק שם חברה בפרמטר 'campaign_name'");
+      }
+  
+      const query = `
+        SELECT 
+          COUNT(*) AS clicks_count
+        FROM ${nameTable}
+        WHERE 
+          event_time >= TIMESTAMP(CURRENT_DATE())
+          AND engagement_type = 'click'
+          AND campaign_name = @campaign_name
+      `;
+  
+      const options = {
+        query,
+        location: "US",
+        params: { campaign_name },
+      };
+  
+      const [job] = await bigquery.createQueryJob(options);
+      const [rows] = await job.getQueryResults();
+  
+      const count = rows[0]?.clicks_count || 0;
+      
+      res.type("text/plain").send(count.toString());
+    } catch (err) {
+      console.error("😒 בעיה בביצוע השאילתה", err);
+      res.status(500).send("אירעה שגיאה בעת ביצוע השאילתה");
+    }
+  };
+  exports.getTodayClicks= async (req, res) => {
+    try {
+      const campaign_name = req.query.campaign_name;
+  
+      if (!campaign_name) {
+        return res.status(400).send("יש לספק שם חברה בפרמטר 'campaign_name'");
+      }
+  
+      const query = `
+        SELECT 
+          COUNT(*) AS clicks_count
+        FROM ${nameTable}
+        WHERE 
+          event_time >= TIMESTAMP(CURRENT_DATE())
+          AND engagement_type = 'click'
+      `;
+  
+      const options = {
+        query,
+        location: "US",
+      };
+  
+      const [job] = await bigquery.createQueryJob(options);
+      const [rows] = await job.getQueryResults();
+  
+      const count = rows[0]?.clicks_count || 0;
+      
+      res.type("text/plain").send(count.toString());
+    } catch (err) {
+      console.error("😒 בעיה בביצוע השאילתה", err);
+      res.status(500).send("אירעה שגיאה בעת ביצוע השאילתה");
+    }
+  };
+  
