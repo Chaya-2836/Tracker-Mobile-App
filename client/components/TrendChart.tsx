a46ade83eb5a788ac6fc9edd28430272ef48d132
Shira <syyelenik@gmail.com>
2025-06-18 13:22:53 +0300

    responsivity

--- client/components/TrendChart.tsx
+++ client/components/TrendChart.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, useWindowDimensions } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
-import { Dimensions } from 'react-native';
 import styles, { chartConfig } from '../app/styles/trendChartStyles';
 
 interface TrendChartProps {
@@ -9,24 +8,26 @@ interface TrendChartProps {
   data: number[];
 }
 
-const screenWidth = Dimensions.get('window').width;
-
 const TrendChart: React.FC<TrendChartProps> = ({ title, data }) => {
+  const { width: screenWidth } = useWindowDimensions(); // auto-updates on resize/rotation
+
   return (
     <View style={styles.chartContainer}>
       <Text style={styles.title}>{title}</Text>
-      <LineChart
-        data={{
-          labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-          datasets: [{ data }],
-        }}
-        width={screenWidth - 40}
-        height={220}
-        chartConfig={chartConfig}
-        style={styles.chart}
-      />
+<LineChart
+  data={{
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [{ data }],
+  }}
+  width={screenWidth * 0.9} // 90% of screen to match container minus padding
+  height={220}
+  chartConfig={chartConfig}
+  style={styles.chart}
+/>
+
     </View>
   );
 };
 
 export default TrendChart;
+
