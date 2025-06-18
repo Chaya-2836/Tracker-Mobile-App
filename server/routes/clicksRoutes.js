7c268b8fce4d45f62daf129eec66e72757d74834
EstiZelt <e0527185161@gmail.com>
2025-06-18 11:01:43 +0300

    add todayClicks

--- server/routes/clicksRoutes.js
+++ server/routes/clicksRoutes.js
@@ -5,5 +5,7 @@ const clicksController = require("../controllers/clicksController");
 
 router.get("/allClicks", clicksController.getAllClicks);
 router.get("/ClicksByCampaign", clicksController.getClicksByCampaign_name);
+router.get("/todayClicksByCampaign", clicksController.getTodayClicksByCampaign);
+router.get("/todayClicks", clicksController.getTodayClicks);
 
 module.exports = router;
