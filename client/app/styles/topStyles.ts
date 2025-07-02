a8faffb1068680cd949b981923fdc33d04198608
michalkarmely <karmelymichal@gmail.com>
2025-07-02 10:58:19 +0300

    tabs

--- client/app/styles/topStyles.ts
+++ client/app/styles/topStyles.ts
@@ -1,10 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    flex: 1,
-  },
+
   selector: {
     flexDirection: 'row',
     alignItems: 'center',
