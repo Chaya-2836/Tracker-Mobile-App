ad47b26b5110e734a0a0b2d6206783e58641873f
michalkarmely <karmelymichal@gmail.com>
2025-07-16 12:40:00 +0300

    logo

--- client/components/Spinner.tsx
+++ client/components/Spinner.tsx
@@ -1,3 +1,23 @@
+
+import React from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
+
+export default function Spinner() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#007bff" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
 // // import React, { useEffect, useRef } from 'react';
 // // import { View, Animated, StyleSheet, Dimensions, Image } from 'react-native';
 // // import { LinearGradient } from 'expo-linear-gradient';
